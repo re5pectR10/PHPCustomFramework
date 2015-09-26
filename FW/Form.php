6340f5a0625cb518c9626c36d1466e46900b045e
re5pectR10 <fc_cska@yahoo.com>
2015-09-26 03:06:27 +0300

    add Form helpers methods

--- FW/Form.php
+++ FW/Form.php
@@ -4,28 +4,73 @@ namespace FW;
 
 class Form {
 
-    public static function open($options = array()) {
-        $method = 'post';
-        $action = '';
-        $name = '';
-        $class = '';
-        $id = '';
-        foreach($options as $key => $value) {
-            $$key = $value;
+    public static function open(array $options = array()) {
+        if (!array_key_exists('method', $options)) {
+            $options['method'] = 'POST';
         }
 
-        return '<form id="'.$id.'" class="'.$class.'" name="'.$name.'" method="'.$method.'" action="'.$action.'">';
+        return '<form' . self::getAttributesAsString($options) . '>';
     }
 
     public static function close() {
         return '</form>';
     }
 
-    public static function text($id = '', $class = '', $name = '') {
-        return self::getInputFormElement('text', $id, $class, $name);
+    public static function text(array $options = array()) {
+        return self::getInputFormElement('text', $options);
     }
 
-    private static function getInputFormElement($type, $id = '', $class = '', $name = '') {
-        return '<input id="'.$id.'" class="'.$class.'" type="'.$type.'" name="'.$name.'">';
+    public static function radio(array $options = array()) {
+        return self::getInputFormElement('radio', $options);
+    }
+
+    public static function check(array $options = array()) {
+        return self::getInputFormElement('checkbox', $options);
+    }
+
+    public static function password(array $options = array()) {
+        return self::getInputFormElement('password', $options);
+    }
+
+    public static function hidden(array $options = array()) {
+        return self::getInputFormElement('hidden', $options);
+    }
+
+    public static function submit(array $options = array()) {
+        return self::getInputFormElement('submit', $options);
+    }
+
+    public static function textarea(array $options = array()) {
+        return '<textarea' . self::getAttributesAsString($options) . '></textarea>';
+    }
+
+    public static function select(array $options = array(), array $elements = array()) {
+        $output = '<select' . self::getAttributesAsString($options) . '>';
+        foreach ($elements as $el) {
+            $output .= '<option';
+            $output .= isset($el['options']) ? self::getAttributesAsString($el['options']) : '';
+            $output .= '>';
+            $output .= isset($el['text']) ? $el['text'] : '';
+            $output .= '</option>';
+        }
+        $output .= '</select>';
+        return $output;
+    }
+
+    public static function csrf() {
+
+    }
+
+    private static function getInputFormElement($type, array $options = array()) {
+        return '<input type="' . $type . '"' . self::getAttributesAsString($options) . '>';
+    }
+
+    private static function getAttributesAsString(array $attr = array()) {
+        $attributes = '';
+        foreach($attr as $key => $value) {
+            $attributes .= ' ' . $key . '="' . $value . '"';
+        }
+
+        return $attributes;
     }
 } 
--- app/views/test.php
+++ app/views/test.php
@@ -4,9 +4,11 @@
     <title></title>
 </head>
 <body><?php //var_dump(get_defined_vars()); ?>
-<?php echo \FW\Form::open(array('action'=>\FW\Common::getBaseURL().'/users/test/asd')) ?>
-    <input type="text" name="a">
-    <input type="text" name="paka">
+<?php
+echo \FW\Form::open(array('action'=>\FW\Common::getBaseURL().'/users/test/asd', 'name'=>'asd')) ;
+echo \FW\Form::text(array('name' => 'a'));
+echo \FW\Form::text(array('name' => 'paka'));
+?>
     <input type="submit">
 </form>
 </body>
